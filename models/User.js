fb1257495d8f1fc1f9fff7ee1737aee1bf8ea4bb
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-20 18:43:03 -0400

    feat: add bcrypt pre-save hook to hash password

--- models/User.js
+++ models/User.js
@@ -22,3 +22,12 @@ const userSchema = new Schema({
     minlength: 8,
   },
 });
+
+// Pre-save hook to hash password
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
