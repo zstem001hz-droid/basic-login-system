8a5ca14afc737168d51c28632b087d6646cd9877
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-20 18:29:02 -0400

    feat: add User schema with username, email, and password fields

---
+++ models/User.js
@@ -0,0 +1,24 @@
+// Requirements
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+// User Schema
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+});
